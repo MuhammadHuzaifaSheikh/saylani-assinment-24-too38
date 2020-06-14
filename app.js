3c8b180d35acaf3c582fd78be2761332dae0cbdd
MuhammadHuzaifaSheikh <you@example.com>
2020-06-14 13:01:37 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,113 @@
+// Chapter 21-38
+
+// Task 1
+
+let firstName=prompt('Type your first name');
+let lastName=prompt('Type your last name');
+let fullName=alert(firstName+' '+lastName)
+
+// Task 2
+
+var favoriteMobile=prompt('Type your favorite Mobil brands');
+  document.write(favoriteMobile.length)
+
+// Task 3
+
+ let findN='pakistani';
+document.write(findN.indexOf('n'))
+
+// Task 4
+
+var helloWorld='Hello World';
+document.write(helloWorld.lastIndexOf('l'))
+
+// Task 5
+
+let firstName=prompt('Type your first name');
+let lastName=prompt('Type your last name');
+let fullName=alert(firstName+' '+lastName)
+
+// Task 6
+
+var favoriteMobile=prompt('Type your favorite Mobil brands');
+  document.write(favoriteMobile.length)
+
+// Task 7
+
+ let findN='pakistani';
+document.write(findN.indexOf('n'))
+
+// / Task 8
+
+let firstName=prompt('Type your first name');
+let lastName=prompt('Type your last name');
+let fullName=alert(firstName+' '+lastName)
+
+// Task 9
+
+var favoriteMobile=prompt('Type your favorite Mobil brands');
+  document.write(favoriteMobile.length)
+
+// Task 10
+
+ let findN='pakistani';
+document.write(findN.indexOf('n'))
+
+// Task 11
+
+var helloWorld='Hello World';
+document.write(helloWorld.lastIndexOf('l'))
+
+// Task 12
+
+let firstName=prompt('Type your first name');
+let lastName=prompt('Type your last name');
+let fullName=alert(firstName+' '+lastName)
+
+// Task 13
+
+var favoriteMobile=prompt('Type your favorite Mobil brands');
+  document.write(favoriteMobile.length)
+
+// Task 14
+
+ let findN='pakistani';
+document.write(findN.indexOf('n'))
+
+// Task 15
+var integer=prompt('please type value in point 1.323 etc');
+document.write(Math.round(+integer);
+
+// Task 16
+
+var random=Math.random()
+document.write(random);
+
+// Task 19
+
+var weight=prompt('Type your weight');
+var possibleWeight=[parseInt(+weight)+0.2+'Kg',parseInt(+weight)+0.6+'Kilo Gram',parseInt(+weight)+'KG']
+console.log(parseInt(+weight));
+document.write(possibleWeight[Math.random()*possibleWeight.length])
+
+
+// Task 21
+
+let findN='pakistani';
+document.write(findN.indexOf('n'))
+
+// Task 22
+var integer=prompt('please type value in point 1.323 etc');
+document.write(Math.round(+integer);
+
+// Task 23
+
+var random=Math.random()
+document.write(random);
+
+// Task 24
+
+var weight=prompt('Type your weight');
+var possibleWeight=[parseInt(+weight)+0.2+'Kg',parseInt(+weight)+0.6+'Kilo Gram',parseInt(+weight)+'KG']
+console.log(parseInt(+weight));
+document.write(possibleWeight[Math.random()*possibleWeight.length])
